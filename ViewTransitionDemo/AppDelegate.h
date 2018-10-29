c62a8caa15327c484c7cdb50d4a12d10d8f0610c
Jasmine <ssnfee@gmail.com>
2018-10-29 09:39:54 +0800

    Initial Commit

---
+++ ViewTransitionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewTransitionDemo
+//
+//  Created by Jasmine on 2018/10/29.
+//  Copyright © 2018 Jasmine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
